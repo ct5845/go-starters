84e49ce6d17473a8e97addb0eb352d6a09df2311
Christopher Turner <cturner@alterian.com>
2024-09-28 08:42:31 +0100

    `starter` example now has templ with live-reloading

--- basics/07_starter/tooling/install/air.toml.sh
+++ basics/07_starter/tooling/install/air.toml.sh
@@ -2,14 +2,29 @@
 
 FILE=".air.toml"
 
-# Update build command
+# Update build command linux
 FROM='cmd = "go build -o .\/tmp\/main ."'
-TO='cmd = "go build -o .\/tmp\/main .\/src\/"'
+TO='cmd = "templ generate \&\& go build -o .\/tmp\/main .\/src\/"'
+sed -i "s|$FROM|$TO|" "$FILE"
+
+# Update build command windows
+FROM='cmd = "go build -o .\/tmp\/main.exe ."'
+TO='cmd = "templ generate \&\& go build -o .\/tmp\/main.exe .\/src\/"'
+sed -i "s|$FROM|$TO|" "$FILE"
+
+# Update include extensions
+FROM='include_ext = \["go", "tpl", "tmpl", "html"\]'
+TO='include_ext = \["go", "tpl", "tmpl", "templ", "html"\]'
 sed -i "s|$FROM|$TO|" "$FILE"
 
 # Update exclude directories
 FROM='exclude_dir = \["assets", "tmp", "vendor", "testdata"\]'
 TO='exclude_dir = \["assets", "tmp", "vendor", "testdata", "node_modules", "dist", "tooling"\]'
+sed -i "s|$FROM|$TO|" "$FILE"
+
+# Update exclude files
+FROM='exclude_regex = \["_test.go"\]'
+TO='exclude_regex = \["_test.go", ".*_templ.go"\]'
 sed -i "s|$FROM|$TO|" "$FILE"
 
 # clean on exit
