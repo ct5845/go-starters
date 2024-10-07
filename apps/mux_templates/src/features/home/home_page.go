a5beaafb1df41a21073e7a9b6d87152275284701
Christopher Turner <cturner@alterian.com>
2024-10-07 14:13:57 +0100

    embed the home_page template - rename the docker image and test

--- apps/mux_templates/src/features/home/home_page.go
+++ apps/mux_templates/src/features/home/home_page.go
@@ -2,24 +2,19 @@ package home
 
 import (
 	"bytes"
+	_ "embed"
 	"html/template"
 	"mux_templates/src/features/ui"
-	"path/filepath"
-	"runtime"
 )
+
+//go:embed home_page.html
+var homePageTemplate string
 
 var homeTemplate *template.Template
 
 func init() {
-	// Get the directory where the current file (home.go) is located
-	_, b, _, _ := runtime.Caller(0)
-	basePath := filepath.Dir(b)
-
-	// Build the path to the template relative to this file
-	templatePath := filepath.Join(basePath, "home_page.html")
-
 	homeTemplate = template.Must(
-		template.ParseFiles(templatePath),
+		template.New("home_page").Parse(homePageTemplate),
 	)
 }
 
